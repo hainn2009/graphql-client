dae637e32e0391622d2d4ecc3b2b50c9ccb43ca9
hainn2009 <hainn2009@gmail.com>
2021-03-17 21:47:31 +0700

    init

---
+++ src/components/PostForm.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import { useForm } from '../util/hooks';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
+function PostForm() {
+    const { values, onChange, onSubmit } = useForm(useFormCallBack, {
+        body: '',
+    });
+
+    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
+        variables: values,
+        // update(_, result) {
+        update(proxy, result) {
+            const data = proxy.readQuery({
+                query: FETCH_POSTS_QUERY,
+            });
+            // data.getPosts = [result.data.createPost, ...data.getPosts];
+            proxy.writeQuery({
+                query: FETCH_POSTS_QUERY,
+                data: { getPosts: [result.data.createPost, ...data.getPosts] },
+            });
+            values.body = '';
+        },
+    });
+    function useFormCallBack() {
+        createPost();
+    }
+
+    return (
+        <>
+            <Form onSubmit={onSubmit}>
+                <Form.Field>
+                    <Form.Input
+                        placeholder="Hi World"
+                        name="body"
+                        onChange={onChange}
+                        value={values.body}
+                        error={error ? true : false}
+                    />
+                    <Button type="submit" color="teal">
+                        Submit
+                    </Button>
+                </Form.Field>
+            </Form>
+            {error && (
+                <div className="ui error message" style={{ marginBottom: 28 }}>
+                    <ui className="list">
+                        <li>{error.graphQLErrors[0].message}</li>
+                    </ui>
+                </div>
+            )}
+        </>
+    );
+}
+//create mutation that call createPost with body parameter get value from $body
+const CREATE_POST_MUTATION = gql`
+    mutation createPost($body: String!) {
+        createPost(body: $body) {
+            id
+            body
+            createAt
+            username
+            likes {
+                id
+                username
+                createAt
+            }
+            likeCount
+            comments {
+                id
+                body
+                username
+                createAt
+            }
+            commentCount
+        }
+    }
+`;
+
+export default PostForm;
---
+++ src/pages/SinglePost.js
@@ -0,0 +1,210 @@
+import React, { useContext, useState, useRef } from 'react';
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import {
+    Card,
+    Grid,
+    Image,
+    Button,
+    Icon,
+    Label,
+    Form,
+} from 'semantic-ui-react';
+import moment from 'moment';
+import LikeButton from '../components/LikeButton';
+import { AuthContext } from '../context/auth';
+
+import DeleteButton from '../components/DeleteButton';
+import MyPopup from '../util/MyPopup';
+
+export default function SinglePost(props) {
+    //TODO: no phai la postId vì trong app.js đã viết như vậy
+    const postId = props.match.params.postId;
+
+    const { user } = useContext(AuthContext);
+    const commentInputRef = useRef(null);
+
+    const [comment, setComment] = useState('');
+
+    const { data: { getPost } = {} } = useQuery(FETCH_POST_QUERY, {
+        variables: { postId },
+    });
+
+    const [submitComment] = useMutation(SUBMIT_COMMENT_MUTATION, {
+        update() {
+            //Set comment to empty after submit it
+            setComment('');
+            commentInputRef.current.blur();
+        },
+        variables: {
+            postId,
+            body: comment,
+        },
+    });
+
+    const deletePostCallback = () => {
+        props.history.push('/');
+    };
+
+    let postMarkup;
+    if (!getPost) {
+        postMarkup = '<p>Loading...</p>';
+    } else {
+        const {
+            id,
+            username,
+            body,
+            createAt,
+            likes,
+            likeCount,
+            comments,
+            commentCount,
+        } = getPost;
+        postMarkup = (
+            <Grid>
+                <Grid.Row>
+                    <Grid.Column width={2}>
+                        <Image
+                            floated="right"
+                            size="small"
+                            src="https://react.semantic-ui.com/images/avatar/large/molly.png"
+                        />
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Card fluid>
+                            <Card.Content>
+                                <Card.Header>{username}</Card.Header>
+                                <Card.Meta>
+                                    {moment(createAt).fromNow()}.
+                                </Card.Meta>
+                                <Card.Description>{body}</Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <LikeButton
+                                    user={user}
+                                    post={{ id, likes, likeCount }}
+                                />
+                                <MyPopup content="Comment on post">
+                                    <Button
+                                        as="div"
+                                        labelPosition="right"
+                                        onClick={() => console.log('Comments')}
+                                    >
+                                        <Button basic color="blue">
+                                            <Icon name="comments" />
+                                        </Button>
+                                        <Label
+                                            basic
+                                            color="blue"
+                                            pointing="left"
+                                        >
+                                            {commentCount}
+                                        </Label>
+                                    </Button>
+                                </MyPopup>
+                                {user && user.username === username && (
+                                    <DeleteButton
+                                        postId={id}
+                                        callback={deletePostCallback}
+                                    />
+                                )}
+                            </Card.Content>
+                        </Card>
+                        {user && (
+                            <Card fluid>
+                                <Card.Content>
+                                    <p>Post a comment</p>
+                                    <Form>
+                                        <div className="ui action input fluid">
+                                            <input
+                                                type="text"
+                                                placeholder="comment.."
+                                                name="comment"
+                                                value={comment}
+                                                onChange={(event) =>
+                                                    setComment(
+                                                        event.target.value
+                                                    )
+                                                }
+                                                ref={commentInputRef}
+                                            />
+                                            <button
+                                                type="submit"
+                                                className="ui button teal"
+                                                disabled={comment.trim() === ''}
+                                                onClick={submitComment}
+                                            >
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </Form>
+                                </Card.Content>
+                            </Card>
+                        )}
+                        {comments.map((comment) => (
+                            <Card fluid key={comment.id}>
+                                <Card.Content>
+                                    {user &&
+                                        comment.username === user.username && (
+                                            <DeleteButton
+                                                postId={postId}
+                                                commentId={comment.id}
+                                            />
+                                        )}
+                                    <Card.Header>
+                                        {comment.username}
+                                    </Card.Header>
+                                    <Card.Meta>
+                                        {moment(comment.createAt).fromNow()}
+                                    </Card.Meta>
+                                    <Card.Description>
+                                        {comment.body}
+                                    </Card.Description>
+                                </Card.Content>
+                            </Card>
+                        ))}
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
+    }
+
+    return postMarkup;
+}
+
+const SUBMIT_COMMENT_MUTATION = gql`
+    mutation($postId: ID!, $body: String!) {
+        createComment(postId: $postId, body: $body) {
+            id
+            comments {
+                id
+                username
+                createAt
+                body
+            }
+            commentCount
+        }
+    }
+`;
+
+const FETCH_POST_QUERY = gql`
+    query($postId: ID!) {
+        getPost(postId: $postId) {
+            id
+            username
+            createAt
+            body
+            likeCount
+            likes {
+                username
+            }
+            commentCount
+            comments {
+                id
+                username
+                createAt
+                body
+            }
+        }
+    }
+`;
