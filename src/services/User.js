9b4772cef29f1aee9a23e31fce00851b09327a71
Nguyen Ngoc Hai <hainn2009@gmail.com>
2023-10-29 10:29:50 +0700

    update

--- src/services/User.js
+++ src/services/User.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-const backendUrl = process.env.NODE_ENV === "development" ? process.env.REACT_APP_URI_DEVELOPMENT : process.env.REACT_APP_URI_PRODUCTION;
+const backendUrl = process.env.NODE_ENV === "development" ? process.env.REACT_APP_URI_PRODUCTION : process.env.REACT_APP_URI_PRODUCTION;
 
 export const login = async ({ username, password }) => {
     try {
         const response = await axios.post(`${backendUrl}/users/login`, { username, password });
         return response.data.data;
     } catch (error) {
-        throw error.response.data.errors;
+        throw error.response.data.error;
     }
 };
 
@@ -19,6 +19,6 @@ export const register = async ({ username, email, password }) => {
         });
         return response.data.data;
     } catch (error) {
-        throw error.response.data.errors;
+        throw error.response.data.error;
     }
 };
